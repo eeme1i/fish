2dbbd0d454b8b54c66bf6e6c555a95f2841e76a6
eemeli ruohomäki <56453212+russhh@users.noreply.github.com>
2022-02-10 09:23:03 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,17 @@ export default function Home() {
   return (
     <div className="bg-nord0 h-screen">
       <Head>
-        <title>Home</title>
-        <meta name="description" content="" />
+        <title>Porin Lukio - Home</title>
+        <meta name="description" content="Porin Lukion (epä)viralliset kotisivut" />
         <link rel="icon" href="" />
       </Head>
 
       <Header />
 
       <div className='font-sans w-auto m-auto lg:w-2/4 sm:w-3/4'>
+        <div className='m-8'>
+        <h1 className='text-4xl font-bold text-nord4'>Porin Lukio</h1>
+        </div>
         <div className='my-16 text-xl font-bold flex justify-center space-x-8 text-nord4'>
           <div className='hover:scale-110 transition ease-in-out hover:bg-nord3'>
             <Link className="" href="/login">
@@ -27,9 +30,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-
       <Footer />
-
     </div>
   )
 }
