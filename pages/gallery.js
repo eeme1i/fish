9299d75b659b14f5abfd6db09de6dee07a29e42c
eemeli ruohomäki <56453212+russhh@users.noreply.github.com>
2022-02-17 14:29:32 +0200

    jotain häikkää pt. 2

--- pages/gallery.js
+++ pages/gallery.js
@@ -5,12 +5,12 @@ import Header from "../components/Header.js";
 
 export default function Home() {
   return (
-    <diV className="bg-nord0 min-h-screen w-full text-nord4">
+    <div className="bg-nord0 min-h-screen w-full text-nord4">
       <Header />
       <p className="w-min mx-auto text-5xl underline p-10">Galleria</p>
       <div className=" w-3/4 md:max-w-4xl mx-auto flex flex-col  space-y-4 py-4 min-h-screen">
         <span className="">
-          "Nyt Tuomas se peli pois, tai pääset seuraavaksi käytävään pelaamaan"{" "}
+          "Nyt Tuomas se peli pois, tai pääset seuraavaksi käytävään pelaamaan"
           <br />
           -Hanna välimäki 16.2.2022
         </span>
@@ -20,6 +20,6 @@ export default function Home() {
         </span>
       </div>
       <Footer />
-    </diV>
+    </div>
   );
 }
