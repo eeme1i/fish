fcbb87937fbf3d6dd629fad7279ed54db364e16d
eemeli ruohomäki <56453212+russhh@users.noreply.github.com>
2022-03-15 10:49:34 +0200

    add text

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,21 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Custom404() {
   return (
     <div className="w-screen h-screen bg-white">
+      <Head>
+        <title>404 ei löydy!!!! </title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="flex w-screen justify-center h-screen items-center">
         <Link href="/home">
           <a>
             <img src="/logo.png" />
+            <p className="text-green-600 serif font-bold w-full justify-center flex">
+              404 ei löydy!!!!
+            </p>
           </a>
         </Link>
       </div>
