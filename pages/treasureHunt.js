34d98bc7715a810d6558821373804a1b463db52a
matikkaeditorinkaantaja <89461562+matikkaeditorinkaantaja@users.noreply.github.com>
2022-03-18 11:58:35 +0200

    Update treasureHunt.js

--- pages/treasureHunt.js
+++ pages/treasureHunt.js
@@ -15,11 +15,6 @@ const imgViewBody = "imageElement";
 // tileAmount, idBody, tileDisplayName, difficulty = 3, scoreDisplay = undefined
 let currentGame = new Game(8, tileIdBody, tileDisplayName, 3, "scoreDisplay");
 
-const tileDisplayName = "Ruutu ";
-const tileIdBody = "treasurePanel"
-// tileAmount, idBody, tileDisplayName, tileImgs[], difficulty = 3, scoreDisplay = undefined
-let currentGame = new Game(8, tileIdBody, tileDisplayName, tileImgs, 3, "scoreDisplay");
-
 let totalPointsCounter = new PointCounter();
 
 const startImage = getImgViewer(0);
@@ -168,7 +163,6 @@ export default function Home() {
   );
 }
 //reactDom.render(elem, document.getElementById('imageElement0'));
-
 
 function startnewgame() {
   // initalize new
