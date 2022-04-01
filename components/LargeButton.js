007d64b52d023080b6776ea75ac39256fc58cdda
eemeli ruohomäki <56453212+eeme1i@users.noreply.github.com>
2022-04-01 21:46:23 +0300

    fix button link and center text

--- components/LargeButton.js
+++ components/LargeButton.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 function LargeButton({ url, value }) {
   return (
-    <div className="hover:scale-110 py-8 bg-nord1 hover:bg-nord2 duration-200 ease-in-out rounded-2xl">
+    <div className="flex justify-center hover:scale-110 bg-nord1 hover:bg-nord2 duration-200 ease-in-out rounded-2xl">
       <Link className="" href={url}>
-        <a className="p-8">{value}</a>
+        <a className="px-8 py-4">{value}</a>
       </Link>
     </div>
   );
