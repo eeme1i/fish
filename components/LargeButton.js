e1a1f98dd163388fe81416183bd60bfea720e1c9
eemeli ruohomäki <56453212+eeme1i@users.noreply.github.com>
2022-04-01 19:52:26 +0300

    fix largebutton

--- components/LargeButton.js
+++ components/LargeButton.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 function LargeButton({ url, value }) {
   return (
     <div className="hover:scale-110 py-8 bg-nord1 hover:bg-nord2 duration-200 ease-in-out rounded-2xl">
-      <Link className="" href="/login">
-        <a className="p-8">kirjautumissivu</a>
+      <Link className="" href={url}>
+        <a className="p-8">{value}</a>
       </Link>
     </div>
   );
