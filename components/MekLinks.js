60e12a09ea5c424e1eb9930a03a74bd6727fa562
matikkaeditorinkaantaja <matikkaeditorinkaantaja@gmail.com>
2022-04-05 21:50:49 +0300

    Add text

--- components/MekLinks.js
+++ components/MekLinks.js
@@ -1,25 +1,36 @@
 import Link from "next/dist/client/link";
 
+function LinkElement({ link, text }) {
+  return (
+    <Link href={link}>
+      <a className="hover:underline mx-auto">{text}</a>
+    </Link>
+  );
+}
+
 export default function MekLinks() {
   return (
-    <div className="pb-8 text-xl text-nord4 flex flex-col gap-3 w-full">
+    <div className="pb-8 p-5 text-xl text-nord4 flex flex-col gap-3 w-full">
       <a className="text-4xl underline mx-auto pb-4">Linkkejä</a>
+      <LinkElement
+        link="https://github.com/matikkaeditorinkaantaja"
+        text="Github sivu"
+      />
 
-      <Link href="https://github.com/matikkaeditorinkaantaja">
-        <a className="hover:underline mx-auto">Github sivu</a>
-      </Link>
+      <LinkElement
+        link="https://github.com/matikkaeditorinkaantaja/Matikkaeditorinkaantaja/releases"
+        text="Uusin versio"
+      />
 
-      <Link href="https://github.com/matikkaeditorinkaantaja/Matikkaeditorinkaantaja/releases">
-        <a className="hover:underline mx-auto">Uusin versio</a>
-      </Link>
+      <LinkElement
+        link="https://docs.google.com/document/d/1akGTKmWnCJrbmtH4gM1ucTExSKc3ipGZCM-l1wJatUQ/edit"
+        text="Ohjeistus"
+      />
 
-      <Link href="https://docs.google.com/document/d/1akGTKmWnCJrbmtH4gM1ucTExSKc3ipGZCM-l1wJatUQ/edit">
-        <a className="hover:underline mx-auto">ohjeistus</a>
-      </Link>
-
-      <Link href="https://docs.google.com/spreadsheets/d/1bi-iejOZ7LSQXTja8hWFj7LcgKMt4z3Aa5pRelak9R8/edit?usp=sharing">
-        <a className="hover:underline mx-auto">Listaus merkkimuutoksista</a>
-      </Link>
+      <LinkElement
+        link="https://docs.google.com/spreadsheets/d/1bi-iejOZ7LSQXTja8hWFj7LcgKMt4z3Aa5pRelak9R8/edit?usp=sharing"
+        text="Listaus merkkimuutoksista"
+      />
     </div>
   );
 }
--- pages/mek.js
+++ pages/mek.js
@@ -4,7 +4,7 @@ import MekLinks from "../components/MekLinks";
 import Head from "next/dist/shared/lib/head";
 
 function Row({ text }) {
-  return <a>&ndash; {text}</a>;
+  return <a className="text-center">{text}</a>;
 }
 
 export default function Home() {
@@ -18,12 +18,34 @@ export default function Home() {
       <Header />
 
       <div className="text-nord4 flex flex-col">
-        <a className="mx-auto py-5 text-5xl underline">
-          Matikkaeditorinkääntäjä
-        </a>
-        <div className="flex flex-col w-sm mx-auto">
-          <a>Mikä?</a>
-          <Row text="kääntää matemaattisen LaTeX -tekstin laskentaohjelmistolle sopivaan muotoon"></Row>
+        <div className="h-28 py-5 text-5xl">
+          <div className="absolute w-full flex">
+            <a className="mx-auto underline visible md:invisible">MEK</a>
+          </div>
+          <div className="absolute w-full flex">
+            <a className="mx-auto underline break-all invisible md:visible">
+              Matikkaeditorinkääntäjä
+            </a>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-2/3 mx-auto gap-2 pb-5">
+          <a className="mx-auto text-2xl">Mikä?</a>
+          <Row text="Matikkaeditorinkääntäjä on ohjelmisto, joka kääntää matemaattisen LaTeX-tekstin laskentaohjelmistolle sopivaan muotoon."></Row>
+          <Row text="Se on tarkoitettu lukiolaisten käyttöön ja sopii erityisen hyvin pitkään matematiikkaan ja fysiikkaan, joissa käytetään paljon pitkiä kaavoja."></Row>
+        </div>
+
+        <div className="flex flex-col w-2/3 mx-auto gap-2 pb-5">
+          <a className="mx-auto text-2xl">Miksi?</a>
+          <Row text="Pitkiä laskentakaavoja on uuvuttava kirjoittaa matikkaeditorista laskentaohjelmistoon. Nyt se hoituu automaattisesti!"></Row>
+          <Row text="Oppitunneilla tulee vähemmän näpyttelyvirheitä, kun kaavoja ei tarvitse siirtää käsin."></Row>
+        </div>
+
+        <div className="flex flex-col w-2/3 mx-auto gap-2 pb-5">
+          <a className="mx-auto text-2xl">Yleistä</a>
+          <Row text="Toimii Windows 7/8.1/10/11 käyttöjärjestelmillä"></Row>
+          <Row text="Täysin ilmainen"></Row>
+          <Row text="Porilaista alkuperää"></Row>
         </div>
       </div>
 
