d979ad4448abe6057553c248e8c9bd5a07679e29
matikkaeditorinkaantaja <89461562+matikkaeditorinkaantaja@users.noreply.github.com>
2022-04-28 10:42:06 +0300

    Finish new fishing design

--- pages/downloadApp.js
+++ pages/downloadApp.js
@@ -1,20 +1,25 @@
 import Head from "next/dist/shared/lib/head";
+import Footer from "../components/Footer.js";
 
 export default function Home() {
   return (
     <div className="w-full min-h-screen bg-blue-300">
-      <div>
-        <Head>
-          <title>Bruh</title>
-          <meta
-            name="description"
-            content="Anna lisää tietoja, jotta voit kirjautua palveluun porinlukio.fi"
-          />
-          <link rel="icon" href="/FakeP.png" />
-        </Head>
+      <Head>
+        <title>Bruh</title>
+        <meta
+          name="description"
+          content="Anna lisää tietoja, jotta voit kirjautua palveluun porinlukio.fi"
+        />
+        <link rel="icon" href="/FakeP.png" />
+      </Head>
+      <div className="w-full min-h-screen">
+        <div className="h-64 w-64 m-auto pt-48">
+          <div className="h-64 w-64 bg-[url('/textures/happyEmojiPng.jpg')] bg-cover "></div>
+          <a className="text-5xl">Kaikki ok...</a>
+        </div>
       </div>
-      <div className="m-auto  h-64 w-64 bg-[url('/textures/happyEmojiPng.jpg')] bg-cover"></div>
-      <a></a>
+
+      <Footer />
     </div>
   );
 }
--- pages/giveSomeMore.js
+++ pages/giveSomeMore.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Link from "next/link";
 
+const poriLogo =
+  "https://fs.pori.fi/adfs/portal/logo/logo.png?id=CB39F2622BCF19EECF330BAF9997244FD33E86E387795B1DE0994342DC3A8A72";
+
 function InputItem({ text, tip }) {
   return (
     <div className="flex flex-col mx-auto">
-      <a className="pt-5 pb-2">{text}</a>
+      <a className="pt-3 pb-2">{text}</a>
       <input
         className="mb-2 pl-1 py-0.5 border border-gray-400 mx-auto"
         placeholder={tip}
@@ -27,39 +30,29 @@ export default function Home() {
         />
         <link rel="icon" href="/FakeP.png" />
       </Head>
-
-      {/* henkilötunnut, pankkikortin numero ja kissan nimi */}
-
       <div className="font-sans text-sm">
         <div className="flex">
-          <div className="h-screen flex bg-cover bg-[url('/illustration.jpg')] w-0 sm:w-1/2"></div>
-
-          <div className="mx-auto mt-8">
+          <div className="h-screen flex bg-cover bg-[url('/illustration.jpg')] w-0 sm:w-3/5"></div>
+          <div className="mx-auto mt-5">
             <div className=" flex flex-col h-full ">
-              <img
-                className="my-6"
-                src="https://fs.pori.fi/adfs/portal/logo/logo.png?id=CB39F2622BCF19EECF330BAF9997244FD33E86E387795B1DE0994342DC3A8A72"
-              />
-              <p className="pb-10 mx-auto text-xl">Anna vielä lisää tietoja</p>
+              <img className="my-3" src={poriLogo} />
+              <p className=" mx-auto text-xl">Anna vielä lisää tietoja</p>
               <InputItem text="henkilötunnus" tip="020504A95H0" />
               <InputItem text="pankkikortin numero" tip="1111-1111-222" />
-              <InputItem
-                text="peniksen pituus senttimetreinä (cm)"
-                tip="esim. 14"
-              />
+              <InputItem text="peniksen pituus (cm)" tip="esim. 14*10¯⁹" />
+              <InputItem text="lemmikkikissan nimi" tip="nahkata-kissa" />
               <InputItem text="osoite" tip="marinaditie 15" />
               <div className="flex flex-col">
                 <div className="flex flex-row mt-5">
                   <input type="checkbox" className="my-1 mr-2" />
                   <a>Haluan myydä sieluni pyövelille</a>
                 </div>
-
                 <div className="flex flex-row mt-5">
                   <input type="checkbox" className="my-1 mr-2" />
                   <a>Haluan tilata mainoskirjeen</a>
                 </div>
               </div>
-              <Link href="/message">
+              <Link href="/downloadApp">
                 <a className="bg-[#0067b8] mx-auto text-white px-5 py-1 mt-10 hover:opacity-25">
                   Nyt sinne sisään!
                 </a>
