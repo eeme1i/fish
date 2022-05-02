cd3bda8e644836aa2049cb3157b1ad9971add172
eemeli ruohomäki <56453212+russhh@users.noreply.github.com>
2022-05-02 12:30:06 +0300

    give some more remove mx-auto

--- pages/giveSomeMore.js
+++ pages/giveSomeMore.js
@@ -6,10 +6,10 @@ const poriLogo =
 
 function InputItem({ text, tip }) {
   return (
-    <div className="flex flex-col mx-auto">
+    <div className="flex flex-col">
       <a className="pt-3 pb-2">{text}</a>
       <input
-        className="mb-2 pl-1 py-0.5 border border-gray-400 mx-auto"
+        className="mb-2 pl-1 py-0.5 border border-gray-400"
         placeholder={tip}
         type="email"
         id="email"
@@ -33,15 +33,15 @@ export default function Home() {
       <div className="font-sans text-sm">
         <div className="flex">
           <div className="h-screen flex bg-cover bg-[url('/illustration.jpg')] w-0 sm:w-3/5"></div>
-          <div className="mx-auto mt-5">
+          <div className="p-16">
             <div className=" flex flex-col h-full ">
               <img className="my-3" src={poriLogo} />
-              <p className=" mx-auto text-xl">Anna vielä lisää tietoja</p>
-              <InputItem text="henkilötunnus" tip="020504A95H0" />
-              <InputItem text="pankkikortin numero" tip="1111-1111-222" />
-              <InputItem text="peniksen pituus (cm)" tip="esim. 14*10¯⁹" />
-              <InputItem text="lemmikkikissan nimi" tip="nahkata-kissa" />
-              <InputItem text="osoite" tip="marinaditie 15" />
+              <p className="text-xl">Anna vielä lisää tietoja</p>
+              <InputItem text="Henkilötunnus" tip="020504A95H0" />
+              <InputItem text="Pankkikortin numero" tip="1111-1111-2222-2222" />
+              <InputItem text="Peniksen pituus (cm)" tip="esim. 14*10¯⁹" />
+              <InputItem text="Lemmikkikissan nimi" tip="nahkata-kissa" />
+              <InputItem text="Osoite" tip="Marinaditie 15" />
               <div className="flex flex-col">
                 <div className="flex flex-row mt-5">
                   <input type="checkbox" className="my-1 mr-2" />
@@ -53,7 +53,7 @@ export default function Home() {
                 </div>
               </div>
               <Link href="/downloadApp">
-                <a className="bg-[#0067b8] mx-auto text-white px-5 py-1 mt-10 hover:opacity-25">
+                <a className="bg-[#0067b8] text-white px-5 py-1 mt-10 hover:opacity-25">
                   Nyt sinne sisään!
                 </a>
               </Link>
