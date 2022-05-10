8f6d7dc5509124df75e4ee57b155ce28994d0618
matikkaeditorinkaantaja <matikkaeditorinkaantaja@gmail.com>
2022-05-10 21:25:40 +0300

    make build possible

--- pages/kurssitarjotin.js
+++ pages/kurssitarjotin.js
@@ -50,8 +50,7 @@ function Cell({ value }) {
 function Leak() {
   let rows = [];
   for (let i = 0; i < leakInfo.length; i++) {
-    console.log(i);
-    rows = [rows, <RowGenerator key={element.id} rowNumber={i} />];
+    rows = [rows, <RowGenerator key={undefined} rowNumber={i} />];
   }
   return (
     <div className=" mx-auto my-10 w-5/6">
@@ -69,14 +68,13 @@ function RowGenerator({ rowNumber }) {
   let infoArray = leakInfo[rowNumber].split(",");
 
   let rowName = infoArray.shift();
-  console.log(rowName);
 
   let componentList = [];
 
   infoArray.forEach((element) => {
     componentList = [
       componentList,
-      <div key={element.id} className="text-sm px-1 m-1 text-nord4 bg-nord0">
+      <div key={undefined} className="text-sm px-1 m-1 text-nord4 bg-nord0">
         {element}
       </div>,
     ];
@@ -104,15 +102,22 @@ export default function Kurssitarjotin() {
 
       <div className="font-sans space-y-8">
         <Header />
-        <Leak />
+        <div className="text-nord4">
+          <h1 className="text-4xl font-bold flex justify-center">
+            Porin lukion kurssitarjotin
+          </h1>
+          <h2 className="text-2xl mt-3 font-bold flex justify-center">
+            2022-2023
+          </h2>
+          <Leak />
+        </div>
         <div className="px-8 flex justify-center text-nord4">
           <div className="space-y-8">
-            <h1 className="text-4xl font-bold flex justify-center">
-              Kurssitarjotin 22
-            </h1>
+            <h2 className="text-4xl font-bold flex justify-center">
+              Tuntikiertokaavio
+            </h2>
             <div className="space-y-8">
-              <p>Tuntikiertokaavio</p>
-              <div className="grid grid-cols-6 grid-rows-6 gap-4">
+              <div className="grid grid-cols-6 grid-rows-6 gap-4 ">
                 <p>Tunti</p>
                 <p>Ma</p>
                 <p>Ti</p>
@@ -121,63 +126,39 @@ export default function Kurssitarjotin() {
                 <p>Pe</p>
 
                 <p>08:30-09:45</p>
-                <p>27</p>
+                <p>7</p>
                 <p></p>
-                <p>21</p>
-                <p>22</p>
-                <p>21</p>
+                <p>1</p>
+                <p>2</p>
+                <p>1</p>
 
                 <p>10:05-11:20</p>
-                <p>25</p>
-                <p>22</p>
-                <p>22</p>
-                <p>23</p>
-                <p>23</p>
+                <p>5</p>
+                <p>2</p>
+                <p>2</p>
+                <p>3</p>
+                <p>3</p>
 
                 <p>11:45-13:15</p>
-                <p>24</p>
-                <p>25</p>
-                <p>24</p>
-                <p>25</p>
-                <p>24</p>
+                <p>4</p>
+                <p>5</p>
+                <p>4</p>
+                <p>5</p>
+                <p>4</p>
 
                 <p>13:30-14:45</p>
-                <p>21</p>
-                <p>26</p>
-                <p>26</p>
-                <p>27</p>
-                <p>26</p>
+                <p>1</p>
+                <p>6</p>
+                <p>6</p>
+                <p>7</p>
+                <p>6</p>
 
                 <p>14:50-16:15</p>
-                <p>21</p>
-                <p>28, 28 ti</p>
-                <p>27</p>
-                <p>28, 28 to</p>
-                <p>28, 28 to</p>
-              </div>
-            </div>
-            <div className="space-y-8 w-[56rem]]">
-              <p>Kurssitarjotin</p>
-              <div className="relative h-96">
-                <Image
-                  src="/kurssitarjotin/1.bmp"
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </div>
-              <div className=" relative h-64">
-                <Image
-                  src="/kurssitarjotin/2.bmp"
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </div>
-              <div className="relative h-64">
-                <Image
-                  src="/kurssitarjotin/3.bmp"
-                  layout="fill"
-                  objectFit="cover"
-                />
+                <p>1</p>
+                <p>8, 8 ti</p>
+                <p>7</p>
+                <p>8, 8 to</p>
+                <p>8, 8 to</p>
               </div>
             </div>
           </div>
